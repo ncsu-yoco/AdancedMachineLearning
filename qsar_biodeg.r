8d81b66c0c888f3a09a8aa7f7f92cfeffc75f978
Kapil Somani <kapilsomani.h@gmail.com>
2015-10-06 14:38:33 -0400

    Parameterized loops

--- qsar_biodeg.r
+++ qsar_biodeg.r
@@ -12,9 +12,9 @@ frmla = as.formula(
 # create test and training set
 n_cross_folds <- function(data, n = 2) {
   index <- sample(1:dim(data)[1])
-  datatrain <- data[index[1:floor(dim(data)[1]*(1/n))], ]
-  datatest <- data[index[((ceiling(dim(data)[1]*(1/n))) + 1):dim(data)[1]], ]
-  return (list(training = datatrain, test = datatrain))
+  datatest <- data[index[1:floor(dim(data)[1]*(1/n))], ]
+  datatrain <- data[index[((ceiling(dim(data)[1]*(1/n))) + 1):dim(data)[1]], ]
+  return (list(training = datatrain, test = datatest))
 }
 
 runSVM <- function(curr_kernel, training, test) {
@@ -47,7 +47,7 @@ checkSVMs <- function() {
                             stringsAsFactors = FALSE
   )
   
-  for (i in seq(1, 10, 2)) {              # various n_folds
+  for (i in seq(2, 10, 2)) {              # various n_folds
     sets <- n_cross_folds(dataset, n = i)     # generating training and test sets
     results = lapply(available_kernels, runSVM, sets$training, sets$test)   # running SVM
     for (j in 1:length(results)) {
@@ -56,7 +56,7 @@ checkSVMs <- function() {
     }
   } 
   
-  print (final_table)
+  return (final_table)
 }
 
 #activation functions, numbers of hidden units, numbers of epochs, and numbers of cross-validation folds
@@ -68,13 +68,27 @@ checkNNs <- function() {
   }
   ds = dataset
   ds[,42] = apply(as.matrix(ds[,42]), 1, translate)
-  for (i in seq(1, 5, 2)) {              # various n_folds
-    sets <- n_cross_folds(ds, n = i)
-    #for(h in seq(1,10,3)) {
-      nn = neuralnet(frmla, sets$training, hidden = 3, threshold = 0.01)
-      a = compute(nn, sets$test[,1:41])
-      b = data.frame(original = ds$test[,42], calc = a$net.result)
-      print (b)   
-    #}
-  }
+  nn = neuralnet(frmla, ds, hidden = 5, rep = 2, threshold = 0.01)
+  a = compute(nn, ds[,1:41])
+  b = data.frame(original = ds[,42], calc = a$net.result)
+  print (dim(ds))
+  print (dim(a$net.result))  
+#   for (i in seq(1, 2, 2)) {              # various n_folds
+#     sets <- n_cross_folds(ds, n = i)
+#     #for(h in seq(1,10,3)) {
+#       nn = neuralnet(frmla, sets$training, hidden = 3, rep = 3, threshold = 0.01)
+#       a = compute(nn, sets$test[,1:41])
+#       #b = data.frame(original = ds$test[,42], calc = a$net.result)
+#       print (dim(sets$test))
+#       print (dim(a$net.result))   
+#     #}
+#   }
+}
+
+for (i in seq(2, 10, 2)) {              # various n_folds
+  sets <- n_cross_folds(dataset, n = i) 
+  print (dim(sets$training))
+  print (dim(sets$test))
+  print (i)
+  print ("**")
 }
